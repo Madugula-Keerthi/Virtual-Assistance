88f8f78073c8ad00904f3b4e3532d8f862e8619e
Madugula-Keerthi <157406470+Madugula-Keerthi@users.noreply.github.com>
2025-06-13 09:59:58 +0530

    Update final_code.py

--- final_code.py
+++ final_code.py
@@ -1,38 +1,34 @@
+import time
 import pyttsx3
-import speech_recognition as sr
 import datetime
+import speech_recognition as sr
 import wikipedia
 import webbrowser
 import os
-import random
-import time
-import pyautogui
-
-from email.message import EmailMessage
-import ssl
+import subprocess
 import smtplib
 
-from googlesearch import search
 
-engine=pyttsx3.init('sapi5')
+engine = pyttsx3.init('sapi5')
+
 voices=engine.getProperty('voices')
-engine.setProperty('voice', voices[0].id)
+engine.setProperty('voice',voices[1].id)
+engine.setProperty('rate', 150)  
 
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
 def wishMe():
-    hour=int(datetime.datetime.now().hour)
-
-    if hour>=0 and hour<12:
+    hour = int(datetime.datetime.now().hour)
+    if hour >= 0 and hour < 12:
         speak("Good Morning!")
-    elif hour>=12 and hour<16:
+    elif hour >= 12 and hour < 18:
         speak("Good Afternoon!")
     else:
         speak("Good Evening!")
-    speak("I am Jack, your virtual assistant.")
 
+    speak("I am Keerthi Assistant. How can I help you?")
 def username():
     speak("What should i call you")
     uname = takeCommand()
@@ -41,114 +37,52 @@ def username():
      
     speak("How can I help you")
 
-def tellDay():
-	
-	day = datetime.datetime.today().weekday() + 1
-	
-	Day_dict = {1: 'Monday', 2: 'Tuesday',
-				3: 'Wednesday', 4: 'Thursday',
-				5: 'Friday', 6: 'Saturday',
-				7: 'Sunday'}
-	
-	if day in Day_dict.keys():
-		day_of_the_week = Day_dict[day]
-		print("The day is ",day_of_the_week)
-		speak("The day is " + day_of_the_week)
-
 def takeCommand():
-    r=sr.Recognizer()
+    r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
-        r.pause_threshold=1
+        r.pause_threshold = 1
         audio=r.listen(source)
-
+    
     try:
-        print("Recognizing...")
+        print("Recognizing....")
         query=r.recognize_google(audio, language='en-in')
         print(f"User said: {query}\n")
     except Exception as e:
-        print("Please say it again")
+        print("Say that again please...")
         return "None"
-    return query 
+    return query
 
 
-def sendEmail(content):
-    email_sender = 'talkwithfayaz@gmail.com'
-    email_password = 'zelplqchdsckowoi'
+def sendEmail(to, content):
+    try:
+        print("Sending email...")
+        sender_email = "maadugulakeerthi2@gmail.com"
+        sender_password = "zecggjwwjmrhlndz"
 
-    email_receiver='maadugulakeerthi2@gmail.com'
+        with smtplib.SMTP('smtp.gmail.com', 587) as server:
+            server.starttls()
+            server.login(sender_email, sender_password)
+            server.sendmail(sender_email, to, content)
+        print("Email sent successfully.")
+    except Exception as e:
+        print(e)
+        speak("Sorry, I was unable to send the email.")
+        print("Error while sending email:", e)
 
-    subject = "Sample"
-    body = content
 
-    em=EmailMessage()
-    em['From']=email_sender
-    em['To']=email_receiver
-    em['subject']=subject
-    em.set_content(body)
-
-    context = ssl.create_default_context()
-
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
-        smtp.login(email_sender, email_password)
-        smtp.sendmail(email_sender, email_receiver, em.as_string())
-
-if __name__ == "__main__":
+if __name__=="__main__":
     wishMe()
-    # username()
-    # while True:
-    if 1:
-        query=takeCommand().lower()
-
+    username()
+    while True:
+        query =takeCommand().lower()
         if 'wikipedia' in query:
             speak('Searching Wikipedia...')
-            query=query.replace("wikipedia", "")
+            query=query.replace("wikipedia","")
             results=wikipedia.summary(query, sentences=2)
             speak("According to wikipedia")
             print(results)
             speak(results)
-
-        elif 'google' in query:
-            speak('Searching in Google...')
-            query=query.replace("google", "")
- 
-        elif 'open youtube' in query:
-            print("Opening YouTube\n")
-            speak("Opening YouTube")
-            webbrowser.open("youtube.com")
-        elif 'open google' in query:
-            print("Opening Google\n")
-            speak("Opening Google")
-            webbrowser.open("google.com")
-
-        elif 'open vs code' in query:
-            print("Opening VS Code\n")
-            speak("Opening VS Code")
-            codePath = "C:\\Users\\Ajay\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
-            os.startfile(codePath)
-        elif 'open chrome' in query: 
-            print("Opening Chrome\n")
-            speak("Opening Chrome")
-            codePath = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
-            os.startfile(codePath)    
-
-        elif 'play music' in query or 'play song' in query:
-            music_dir = 'C:\\Users\\Ajay\\OneDrive\\Desktop\\Fayaz\\Songs'
-            songs=os.listdir(music_dir)
-            os.startfile(os.path.join(music_dir, songs[random.randint(0, 7)]))
-
-        elif 'day' in query or 'today' in query:
-            tellDay()
-        elif 'the time' in query:
-            strTime = datetime.datetime.now().strftime("%H:%M:%S")
-            print("Time is ",strTime) 
-            speak(f"The time is {strTime}")
-
-        elif 'bye' in query:
-            print("See you later. Have a nice day.\n")
-            speak("See you later. Have a nice day.")
-            exit()
-
         elif 'how are you' in query:
             print("I am fine Thank you\n")
             speak("I am fine Thank you")
@@ -162,46 +96,20 @@ if __name__ == "__main__":
         elif "who made you" in query or "who created you" in query:
             print("I have been created by team of software engineers\n")
             speak("I have been created by team of software engineers")
-        
-        elif "will you be my gf" in query or "will you be my bf" in query:
-            print("I'm not sure about, may be you should give me some time\n")
-            speak("I'm not sure about, may be you should give me some time")
 
-        elif "i love you" in query:
-            print("It's hard to understand\n")
-            speak("It's hard to understand")
-
-        elif 'email' in query or 'send mail' in query:
-            try:
-                print("What should I send?\n")
-                speak("What should I send?")
-                content=takeCommand()
-                sendEmail(content)
-                print("Email has been sent!\n")
-                speak("Email has been sent!")
-            except Exception as e:
-                speak("Sorry, I'm not able to send this email\n")
-
-        elif 'note' in query:
-            print("What should I note?\n")
-            speak("What should I note?")
-            text=takeCommand()
-            fh=open('text.txt','w')
-            fh.write(text)
-            fh.close()
-            print("Notes have been taken!\n")
-            speak("Notes have been taken!")
-        elif 'news' in query:
-            print("Displaying News\n")
-            speak("Displaying News")
-            webbrowser.open("https://news.google.com/home?gl=IN&hl=en-IN&ceid=IN:en")
-
-        elif 'screenshot' in query:
-            img=pyautogui.screenshot()
-            img.save(r"C:\\Users\\Ajay\\OneDrive\\Desktop\\Fayaz\\Project Jack\\image1.png")
-            print("Screenshot Taken!\n")
-            speak("Screenshot Taken!")
-
+        elif 'open youtube' in query:
+            print("Opening YouTube\n")
+            speak("Opening YouTube")
+            webbrowser.open("youtube.com")
+        elif 'open google' in query:
+            print("Opening Google\n")
+            speak("Opening Google")
+            webbrowser.open("google.com")
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")
+        elif 'play music' in query:
+            song_path = r"C:\Users\sivac\Music\[iSongs.info] 01 - Langa Vonilo.mp3"
+            subprocess.Popen(['C:\\Program Files\\Windows Media Player\\wmplayer.exe', song_path])
         elif 'joke' in query:
             jk = random.randint(0, 5)
             if jk == 0:
@@ -239,17 +147,25 @@ if __name__ == "__main__":
                 speak("I'm so good at sleeping I can do it with my eyes closed!")
 
             speak("hahhahaha")
-        elif 'set timer' in query or 'timer' in query or ' countdown ' in query:
-            print("For How Many Seconds ")
-            speak("For How Many Seconds ")
-            sec =int(takeCommand())
-            while sec>0:
-                print(sec)
-                speak(sec)
-                sec -= 1
-                time.sleep(1)
-            print("Time's Up\n")
-            speak("Time's Up")
+        elif 'time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+            speak(f"Sir, the time is {strTime}")
+        elif 'send email' in query:
+            try:
+                speak("What should I say?")
+                content = takeCommand()
+                print("Captured content:", content)  # Log the content
+                to = "maadugulakeerthi@gmail.com"
+                sendEmail(to, content)
+                speak("Email has been sent successfully.")
+            except Exception as e:
+                print("Error while sending email:", e)
+                speak("Sorry, I was unable to send the email.")
+        elif 'exit'  in query:
+            print("See you later. Have a nice day.\n")
+            speak("See you later. Have a nice day.")
+            print("Exiting assistant...")
+            exit()
         else:
             print("Oops! My Team forgot to add this function in me ,please try another \n")
             speak("Oops! My Team forgot to add this function in me ,please try another ")
